69ef98a3a85343457a96860b559576603edfb2e4
Frank Langner <dliwespf@posteo.de>
2021-03-10 00:57:37 +0100

    Not compiling - Todo: change ownership of fields in Dice

--- roll/src/dice/dice.rs
+++ roll/src/dice/dice.rs
@@ -1,5 +1,6 @@
 use super::rollable::Rollable;
 use rand::seq::SliceRandom;
+use std::convert::TryFrom;
 
 pub struct Dice <'v, A> {
     sides: &'v Vec<A>,
@@ -27,5 +28,22 @@ impl <'v, A> Rollable<'v, A> for Dice<'v, A> {
     fn roll(&'v mut self) -> &A {
         self.upside = self.sides.choose(&mut rand::thread_rng()).unwrap();
         self.upside
+    }
+}
+
+impl <'v> TryFrom<&'v str> for Dice<'v, u16> {
+    type Error = String;
+
+    fn try_from(formula: &'v str) -> Result<Dice<'v, u16>, Self::Error> {
+        let min_value: u16 = match formula.chars().next() {
+            Some('d') => Ok(0),
+            Some('D') => Ok(1),
+            _ => Err(format!("'{}' is not a valid dice formula: Does not start with 'd' or 'D'", formula.to_string()))
+        }?;
+
+        let sides: u16  = formula[1..].parse::<u16>().or(Err(format!("'{}' is not a valid dice formula: Number not parsable", formula)))?;
+
+        let sides_vec: &'v Vec<u16> = (min_value..(sides - 1 + min_value)).collect();
+        Dice::new(&sides_vec)
     }
 }
