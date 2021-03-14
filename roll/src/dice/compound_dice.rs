0e694cf67105d2aedb669e1c5f09d74c7c8d151d
Frank Langner <dliwespf@posteo.de>
2021-03-14 15:56:25 +0100

    Added failing tests for CompoundDice::roll()

--- roll/src/dice/compound_dice.rs
+++ roll/src/dice/compound_dice.rs
@@ -70,9 +70,9 @@ mod tests {
         assert!(compound_dice_1_result.is_ok());
 
         let dices2: Vec<Box<Rollable<u16>>> = vec![
-            Box::new(Dice::new(&(1..6).collect()).unwrap()),
-            Box::new(Dice::new(&(1..6).collect()).unwrap()),
-            Box::new(Dice::new(&(7..20).collect()).unwrap()),
+            Box::new(Dice::new(&(3..7).collect()).unwrap()),
+            Box::new(Dice::new(&(1..100).collect()).unwrap()),
+            Box::new(Dice::new(&(0..29).collect()).unwrap()),
         ];
         let compound_dice_2_result = CompoundDice::new(dices2);
         assert!(compound_dice_2_result.is_ok());
@@ -82,5 +82,59 @@ mod tests {
             Box::new(compound_dice_2_result.unwrap()),
         ];
         assert!(CompoundDice::new(compound_dices).is_ok());
+    }
+
+    #[test]
+    fn roll_with_one_dice() {
+        let sub_dices: Vec<Box<Rollable<u16>>> = vec![Box::new(Dice::new(&(1..6).collect()).unwrap())];
+        let mut dice = CompoundDice::new(sub_dices).unwrap(); // unwrap is safe here; method is untder test a few lines above
+        for _ in (1 .. 200) {
+            let result = dice.roll();
+            assert!(*result >= 1 && *result <= 6)
+        }
+    }
+
+    #[test]
+    fn roll_with_three_dices() {
+        let sub_dices: Vec<Box<Rollable<u16>>> = vec![
+            Box::new(Dice::new(&(1..6).collect()).unwrap()),
+            Box::new(Dice::new(&(1..6).collect()).unwrap()),
+            Box::new(Dice::new(&(7..20).collect()).unwrap()),
+        ];
+        let mut dice = CompoundDice::new(sub_dices).unwrap(); // unwrap is safe here; method is untder test a few lines above
+        for _ in (1 .. 200) {
+            let result = dice.roll();
+            assert!(*result >= 9 && *result <= 32)
+        }
+    }
+
+    #[test]
+    fn roll_with_nested_compound_dice() {
+        let dices1: Vec<Box<Rollable<u16>>> = vec![
+            Box::new(Dice::new(&(1..6).collect()).unwrap()),
+            Box::new(Dice::new(&(1..6).collect()).unwrap()),
+            Box::new(Dice::new(&(7..20).collect()).unwrap()),
+        ];
+        let compound_dice_1_result = CompoundDice::new(dices1);
+        assert!(compound_dice_1_result.is_ok());
+
+        let dices2: Vec<Box<Rollable<u16>>> = vec![
+            Box::new(Dice::new(&(3..7).collect()).unwrap()),
+            Box::new(Dice::new(&(1..100).collect()).unwrap()),
+            Box::new(Dice::new(&(0..29).collect()).unwrap()),
+        ];
+        let compound_dice_2_result = CompoundDice::new(dices2);
+        assert!(compound_dice_2_result.is_ok());
+
+        let compound_dices: Vec<Box<Rollable<u16>>> = vec![
+            Box::new(compound_dice_1_result.unwrap()),
+            Box::new(compound_dice_2_result.unwrap()),
+        ];
+        assert!(CompoundDice::new(compound_dices).is_ok());
+
+        for _ in (1 .. 1000) {
+            let result = compound_dices.roll();
+            assert!(*result >= 13 && *result <= 168)
+        }
     }
 }
